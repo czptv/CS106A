dfab1cc18c50b5b322d1f76a12328d6bd3f25ca7
S <s>
2015-10-17 03:10:54 -0700

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -61,8 +61,178 @@ public class Breakout extends GraphicsProgram {
 
 /* Method: run() */
 /** Runs the Breakout program. */
+	/*
+	 * Create an instance variable of a brick, a paddle and a ball.
+	 */
+	
+	private GRect brick, paddle;
+	private GOval ball;
+	private double vx, vy; // the x component and y component of velocity of ball
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private int brickCount;
+	
 	public void run() {
-		/* You fill this in, along with any subsidiary methods */
+		setup();
+		play();
+			
+	}
+ 
+	private void setup() {
+		drawBricks();
+		drawPaddle();
+		drawBall();
+	}
+	
+	/*
+	 * Draw all the bricks.
+	 */
+	
+	private void drawBricks() {
+		for (int i = 0; i<NBRICK_ROWS; i++) {
+			drawRow(i);
+			colorBrick(i);		
+		}
 	}
 
+	//draw a row of the brick pile
+	
+	private void drawRow(int row) {
+		for(int i = 0; i<NBRICKS_PER_ROW; i++) {
+			drawOneBrick(row, i);
+		}
+	}
+	
+	//create one brick
+	
+	private void drawOneBrick(int row, int col) {
+		double x0 = (WIDTH - NBRICKS_PER_ROW * BRICK_WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP)/2; //the distance of leftmost brick to left border of the world
+		double x = x0 + (BRICK_WIDTH + BRICK_SEP) * col;
+		double y = BRICK_Y_OFFSET + (BRICK_SEP + BRICK_HEIGHT) * row;
+		GRect brick = new GRect (x,y,BRICK_WIDTH, BRICK_HEIGHT);
+		brick.setFilled(true);
+		add(brick);
+	}
+	
+	//set color for the bricks according to which rows they are in.	
+	
+	private void colorBrick(int row) {
+		if (row<2) {
+			brick.setColor(Color.RED);
+		}
+		else if (row<4) {
+			brick.setColor(Color.ORANGE);
+		}
+		else if (row<6) {
+			brick.setColor(Color.YELLOW);
+		}
+		else if (row<8) {
+			brick.setColor(Color.GREEN);
+		}
+		else if(row<10) {
+			brick.setColor(Color.CYAN);
+		}
+	}
+	
+	/*
+	 * Draw a paddle.
+	 */
+	
+	private void drawPaddle() {
+		double x = (WIDTH - PADDLE_WIDTH)/2;
+		double y = HEIGHT - BRICK_Y_OFFSET - PADDLE_HEIGHT;
+		paddle = new GRect(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		add(paddle);
+		addMouseListeners();
+	}
+	
+	/*
+	 * Make the paddle track the movement of user's mouse.
+	 */
+	
+	public void mouseMoved (MouseEvent e) {
+		if ((e.getX() > PADDLE_WIDTH/2) && (e.getX() < WIDTH - PADDLE_WIDTH/2)) {
+		double x = e.getX() - PADDLE_WIDTH/2;
+		double y = HEIGHT - BRICK_Y_OFFSET - PADDLE_HEIGHT;
+		paddle.setLocation(x, y);
+		}
+	}
+	
+	/*
+	 * Draw a ball.
+	 */
+	
+	private void drawBall() {
+		double x = WIDTH/2 - BALL_RADIUS;
+		double y = HEIGHT/2 - BALL_RADIUS;
+		double d = 2 * BALL_RADIUS;
+		ball = new GOval(x, y, d, d);
+		ball.setFilled(true);
+		add(ball);
+	}
+
+	/*
+	 * Users start the game with a click. Ball begins to fall after the click. The game does not end
+	 * until the ball falls off the bottom of the screen or there is no brick left. If you break all
+	 * the bricks before using up the lives, you win. If the ball falls off the bottom of the screen,
+	 * you turn to other round until you use up all your lives.
+	 */
+	
+	
+	private void play() {
+		waitForClick();
+		getVelocity();
+		for (int i=0; i<NTURNS; i++) {
+			completeOneTurn();
+		}
+		add(prompt("YOU LOSER! GAME OVER!")); //prompt for if user lose the game
+	}
+	
+	/*
+	 * After the user clicks, the balls gains an initial speed and starts to move.
+	 */
+	
+	private void getVelocity() {
+		vy = 3.0;
+		vx = rgen.nextDouble(1.0, 3.0);
+		if (rgen.nextBoolean(0.5)) {
+			vx = -vx;
+		}
+	}
+	
+	/*
+	 * Complete one turn of the game until user wins or loses. 
+	 * If user breaks all the bricks, they win.
+	 */
+	
+	private void completeOneTurn() {
+		boolean loseOneTurn=(ball.getY() >= HEIGHT);
+		boolean win=(brickCount == 0);
+		boolean stillAlive= !(loseOneTurn || win);
+		
+		//Continue break the bricks in one turn until user lose or win
+		while(stillAlive) {
+			moveBall();
+		}
+		
+		//if user wins, game over
+		if (win) {
+			add(prompt("You Win!"));
+			break;
+		}
+	}	
+	
+	
+	/*
+	 * prompt at the end of the game to indicate whether the user wins or loses
+	 */
+	
+	private GLabel prompt(String endGame) {
+		GLabel prompt=new GLabel(endGame);
+		prompt.setFont("Times-Bold-50");
+		double x=(WIDTH-prompt.getWidth())/2;
+		double y=HEIGHT*4.0/5.0;
+		prompt.setLocation(x, y);
+		return prompt;
+	}
 }
