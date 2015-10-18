baea58a8cda49002e7cad5118101c29c52e2b7cb
S <s>
2015-10-18 13:42:44 -0700

    {"type":"TI", "runs":"3"}

--- CopyOfBreakout.java
+++ CopyOfBreakout.java
@@ -274,19 +274,6 @@ private static final int DELAY = 50;
 		if (obj == paddle) {
 			vx = -vx;
 		}
-		else if (obj == ball) {
-			int hitTop = checkSide(x, y - BALL_RADIUS); //check top vertex
-			int hitBottom = checkSide(x, y + BALL_RADIUS); //check bottom vertex
-			int hitTotal = hitTop + hitBottom;
-			if (hitTotal != 0) {
-				vx = -vx;
-			}
-		} 
-		else {
-			remove(obj);
-			brickCount--;
-			vx = -vx;
-		}
 	}
 	
 	private int checkSide(double x, double y) {
