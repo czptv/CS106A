1ffe7057a729e47d306667b309895edafdea0179
S <s>
2015-10-18 11:35:13 -0700

    {"type":"TI", "runs":"3"}

--- CopyOfBreakout.java
+++ CopyOfBreakout.java
@@ -246,24 +246,12 @@ private static final int DELAY = 50;
 		GObject obj = getElementAt(x,y);
 		if (obj == paddle) {
 			vy = -vy;
-		}
-		else if (obj ==ball) {
-			checkSide(x - BALL_RADIUS, y); //check left side
-			checkSide(x + BALL_RADIUS, y); //check right side
-		} 
-		else {
 			remove(obj);
 			brickCount--;
 			vy = -vy;
 		}
 	}
-	
-	private void checkSide(double x, double y) {
-		GObject obj = getElementAt(x,y);
-		remove (obj);
-		brickCount--;
-	}
-	
+
 }
 
 
